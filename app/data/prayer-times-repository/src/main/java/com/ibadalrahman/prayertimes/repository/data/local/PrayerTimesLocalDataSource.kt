e2de1a88abb2bb4d69488e4678d770f22e78c794
Ghamza-Jd <hamzah.jadeed@gmail.com>
2025-04-21 17:52:01 +0300

    feat: added week prayer times local database entities

--- app/data/prayer-times-repository/src/main/java/com/ibadalrahman/prayertimes/repository/data/local/PrayerTimesLocalDataSource.kt
+++ app/data/prayer-times-repository/src/main/java/com/ibadalrahman/prayertimes/repository/data/local/PrayerTimesLocalDataSource.kt
@@ -1,11 +1,16 @@
 package com.ibadalrahman.prayertimes.repository.data.local
 
 import com.ibadalrahman.prayertimes.repository.data.local.entities.DayPrayerTimesEntity
+import com.ibadalrahman.prayertimes.repository.data.local.entities.WeekEntity
+import com.ibadalrahman.prayertimes.repository.data.local.entities.WeekPrayerTimesEntity
 
 interface PrayerTimesLocalDataSource {
-    fun insertAll(vararg prayerTimes: DayPrayerTimesEntity)
-    fun findById(id: Int): DayPrayerTimesEntity
+    fun insertDayPrayerTime(vararg prayerTimes: DayPrayerTimesEntity)
+    fun insertWeekPrayerTime(vararg week: WeekEntity)
+    fun findDayPrayerTimeById(id: Int): DayPrayerTimesEntity
+    fun findWeekPrayerTimeById(id: Int): WeekPrayerTimesEntity
     fun getDigest(year: Int): String
     fun setDigest(year: Int, digest: String)
-    fun deleteAll()
+    fun deleteAllDayPrayerTimes()
+    fun deleteAllWeekPrayerTimes()
 }
---
+++ app/data/prayer-times-repository/src/main/java/com/ibadalrahman/prayertimes/repository/data/local/entities/WeekHadithEntity.kt
@@ -0,0 +1,6 @@
+package com.ibadalrahman.prayertimes.repository.data.local.entities
+
+data class WeekHadithEntity(
+    val hadith: String,
+    val note: String?
+)
