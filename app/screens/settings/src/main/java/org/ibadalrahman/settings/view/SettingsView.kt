eac2d823deeca10b3f797b794949383eabf48bbf
MABD <mabd.universe@gmail.com>
2025-11-24 19:41:46 +0200

    use ListItem in theme switcher composable

--- app/screens/settings/src/main/java/org/ibadalrahman/settings/view/SettingsView.kt
+++ app/screens/settings/src/main/java/org/ibadalrahman/settings/view/SettingsView.kt
@@ -1,6 +1,5 @@
 package org.ibadalrahman.settings.view
 
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -17,26 +16,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.OpenInNew
-import androidx.compose.material.icons.filled.ChevronLeft
 import androidx.compose.material.icons.filled.Contrast
 import androidx.compose.material.icons.filled.FontDownload
 import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.OpenInNew
-import androidx.compose.material.icons.filled.OpenInNewOff
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.SupportAgent
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextFieldDefaults.contentPadding
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -49,10 +40,10 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import org.ibadalrahman.resources.R
 import org.ibadalrahman.settings.presenter.entity.Language
 import org.ibadalrahman.settings.presenter.entity.SettingsIntention
+import org.ibadalrahman.settings.repository.data.domain.Theme
 
 @Composable
 fun SettingsView(
@@ -93,11 +84,10 @@ fun SettingsView(
             Column(modifier = Modifier
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(12.dp))
-                .background(MaterialTheme.colorScheme.background)
             ) {
                 LanguageSelector(intentionProcessor = intentionProcessor)
-//            HorizontalDivider()
-//            ThemeSelector(intentionProcessor = intentionProcessor)
+                Spacer(Modifier.height(1.dp))
+                ThemeSelector(intentionProcessor = intentionProcessor)
             }
         }
 
@@ -223,7 +213,6 @@ fun LanguageSelector(intentionProcessor: (intention: SettingsIntention) -> Unit)
                 }
             },
             modifier = Modifier
-                .clip(RoundedCornerShape(12.dp))
                 .clickable(onClick = { expanded = !expanded }),
         )
     }
@@ -234,67 +223,60 @@ fun ThemeSelector(intentionProcessor: (intention: SettingsIntention) -> Unit) {
     var expanded by remember { mutableStateOf(false) }
 
     Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 20.dp, vertical = 10.dp)
-            .clickable { expanded = !expanded }
+        modifier = Modifier.fillMaxWidth()
     ) {
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.background)
-        ) {
-            Icon(
-                imageVector = Icons.Default.Contrast,
-                contentDescription = stringResource(id = R.string.language),
-                modifier = Modifier.padding(end = 16.dp),
-            )
-            Text(
-                text = stringResource(id = R.string.appearance),
-                style = MaterialTheme.typography.bodyLarge.copy(
-                    color = MaterialTheme.colorScheme.onPrimaryContainer,
-                    fontWeight = FontWeight.Medium
-                ),
-            )
-            Spacer(modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.background)
-            )
-            Box {
-                Icon(
-                    imageVector = Icons.Default.KeyboardArrowDown,
-                    contentDescription = stringResource(id = R.string.select),
+        ListItem(
+            headlineContent = {
+                Text(
+                    text = stringResource(id = R.string.appearance),
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        fontWeight = FontWeight.Medium
+                    ),
                 )
-                DropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false }
-                ) {
-                    DropdownMenuItem(
-                        text = { Text("Dark") },
-                        onClick = {
-                            intentionProcessor(
-                                SettingsIntention.ChangeTheme(AppCompatDelegate.MODE_NIGHT_YES)
-                            )
-                        }
+            },
+            leadingContent = {
+                Icon(
+                    imageVector = Icons.Default.Contrast,
+                    contentDescription = stringResource(id = R.string.language),
+                )
+            },
+            trailingContent = {
+                Box {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowDown,
+                        contentDescription = stringResource(id = R.string.select),
                     )
-                    DropdownMenuItem(
-                        text = { Text("Light") },
-                        onClick = {
-                            intentionProcessor(
-                                SettingsIntention.ChangeTheme(AppCompatDelegate.MODE_NIGHT_NO)
-                            )
-                        }
-                    )
-                    DropdownMenuItem(
-                        text = { Text("System") },
-                        onClick = {
-                            intentionProcessor(
-                                SettingsIntention.ChangeTheme(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-                            )
-                        }
-                    )
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        DropdownMenuItem(
+                            text = { Text("Dark") },
+                            onClick = {
+                                expanded = false
+                                intentionProcessor(SettingsIntention.ChangeTheme(Theme.Dark))
+                            }
+                        )
+                        DropdownMenuItem(
+                            text = { Text("Light") },
+                            onClick = {
+                                expanded = false
+                                intentionProcessor(SettingsIntention.ChangeTheme(Theme.Light))
+                            }
+                        )
+                        DropdownMenuItem(
+                            text = { Text("System") },
+                            onClick = {
+                                expanded = false
+                                intentionProcessor(SettingsIntention.ChangeTheme(Theme.System))
+                            }
+                        )
+                    }
                 }
-            }
-        }
+            },
+            modifier = Modifier
+                .clickable { expanded = !expanded },
+        )
     }
 }
